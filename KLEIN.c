7d6d56d3879842e5dc666af727aadd3be1694608
Joost Rijneveld <joostrijneveld@gmail.com>
2014-02-18 13:34:51 +0100

    AddRoundkey and Subnibbles working

--- KLEIN.c
+++ KLEIN.c
@@ -1,21 +1,55 @@
+#include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 
-void print8b(char* str) {
-	int j;               // as they are not used outside of main
-	for (j = 0; j < 8; j++ ) {
-		printf( "--%X\n", str[j] & 0xFF );
+const uint64_t KEY = 0x0000000000000000;
+const uint64_t PLAINTEXT = 0x1234567890ABCDEF;
+const int Nr = 12;
+
+// const int SBOX[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+const int SBOX[] = {0x7, 0x4, 0xA, 0x9, 0x1, 0xF, 0xB, 0x0, 0xC, 0x3, 0x2, 0x6, 0x8, 0xD, 0xE, 0x5};
+
+void printBin(uint64_t n) {
+	int i;
+	for (i = 63; i >= 0; i--) {
+		printf("%d", (int) (n >> i) & 0x1);
+		if (i % 4 == 0) {
+			printf(" ");
+		}
 	}
+	printf("\n");
+}
+
+uint64_t addRoundKey(uint64_t state, uint64_t sk) {
+	return state ^ sk;
+}
+
+uint64_t subNibbles(uint64_t state) {
+	uint64_t result = 0;
+	int i;
+	for (i = 15; i >= 0; i--) {
+		result = result << 4;
+		result += SBOX[(state & ((uint64_t) 0xF << 4*i)) >> 4*i];
+		printBin(result);
+	}
+	return result;
+}
+
+uint64_t KLEIN(uint64_t key, uint64_t plaintext) {
+	uint64_t sk = key;
+	uint64_t state = plaintext;
+	int i;
+	for (i = 0; i < Nr; i++) {
+		state = addRoundKey(state, sk);
+		state = subNibbles(state);
+		break;
+	}
+	return 0;
 }
 
 int main() {
-	// char key[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	// char plaintext[] = {0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x00};
-	// char str[] = {'B','y','e','\0'}; 
-	// printf('%c', str[0]);
-	
-	char key[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	char plaintext[] = {0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF};
-	print8b(plaintext);
-	
+	printf("Key: %" PRIX64 "\n", KEY);
+	printf("Plaintext: %" PRIX64 "\n", PLAINTEXT);
+	printf("Ciphertext: %" PRIX64 "\n", KLEIN(KEY, PLAINTEXT));
 	return 0;
 }
